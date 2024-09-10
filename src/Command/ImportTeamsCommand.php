7f29d7eed05d6e740025c92924a76e7ff2de6f53
FelixKlei1992 <felix.kleibel@gmail.com>
2024-09-10 13:18:51 +0200

    Add CLI command

--- src/Command/ImportTeamsCommand.php
+++ src/Command/ImportTeamsCommand.php
@@ -31,27 +31,40 @@ class ImportTeamsCommand extends Command
 
         $output->writeln('Importing data from ' . $filePath);
 
+        //load Excel-file
         $spreadsheet = IOFactory::load($filePath);
         $sheet = $spreadsheet->getActiveSheet();
+
+        //convert table into array
         $data = $sheet->toArray();
 
-        $headers = array_shift($data);
-
-        foreach ($data as $row) {
-            $teamData = array_combine($headers, $row);
-
-            $team = new Team();
-            $team->setKey($teamData['team_name']);
-            $team->setTeamName($teamData['team_name']);
-            $team->setTeamFoundingDate(new \DateTime($teamData['team_foundingDate']));
-            $team->setTeamTrainer($teamData['team_trainer']);
-            
-            $team->save();
-
-            $output->writeln('Imported team: ' . $teamData['team_name']);
+        //check for data
+        if (count($data) <= 1) {
+            $output->writeln('<error>No data found in the file.</error>');
+            return Command::FAILURE;
         }
 
-        $output->writeln('Import complete.');
+        //extract table-field-description from first row
+         $headers = array_shift($data);
+         $output->writeln('<info>Headers: ' . implode(', ', $headers) . '</info>');
+
+         //iterate through the rest of the lines and output data (in console)
+         foreach ($data as $row) {
+             $teamData = array_combine($headers, $row);
+
+
+
+             $team = new Team();
+             $team->setKey($teamData['team_name']);
+             $team->setTeamName($teamData['team_name']);
+             $team->setTeamFoundingDate($teamData['team_foundingDate']);
+             $team->setTeamTrainer($teamData['team_trainer']);
+             $team->save();
+
+             $output->writeln('Imported team: ' . $teamData['team_name'] . ' foundingDate: ' . $teamData['team_foundingDate'] . ' Trainer: ' . $teamData['team_trainer']);
+        }
+
+         $output->writeln('Import complete.');
         return Command::SUCCESS;
     }
 }
