832ef038152a9dde17f3a8747d7931a5fb245b31
FelixKlei1992 <felix.kleibel@gmail.com>
2024-09-11 16:31:38 +0200

    changes in git ignor

---
+++ vendor/knplabs/knp-components/src/Knp/Component/Pager/Event/BeforeEvent.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Knp\Component\Pager\Event;
+
+use Doctrine\DBAL\Connection;
+use Knp\Component\Pager\ArgumentAccess\ArgumentAccessInterface;
+use Symfony\Contracts\EventDispatcher\Event;
+use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
+
+/**
+ * Specific Event class for paginator
+ */
+final class BeforeEvent extends Event
+{
+    public function __construct(
+        private readonly EventDispatcherInterface $eventDispatcher,
+        private readonly ArgumentAccessInterface $argumentAccess,
+        private readonly ?Connection $connection = null
+    ) {
+    }
+
+    public function getEventDispatcher(): EventDispatcherInterface
+    {
+        return $this->eventDispatcher;
+    }
+
+    public function getArgumentAccess(): ArgumentAccessInterface
+    {
+        return $this->argumentAccess;
+    }
+
+    public function getConnection(): ?Connection
+    {
+        return $this->connection;
+    }
+}
